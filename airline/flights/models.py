008fcfd62571f85db66421c8c080f6a110783703
“Lee <leesheppard2404@gmail.com>
2022-10-29 14:15:21 +0200

    finished lecture

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,3 +1,27 @@
+from distutils.command.build_scripts import first_line_re
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+class Flight(models.Model):
+    # this code means that if an airport is deleted, so will the flights from that airport (Airport, on_delete=models.CASCADE)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+from .models import Flight, Passenger
 
 # Create your views here.
+def index(request):
+    context =  {
+        "flights": Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+
+    context = {
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+    }
+
+    return render(request, "flights/flight.html", context)
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight) 
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
+
